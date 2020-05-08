5e3f21fe86eda41a2d30958e58826c817f0f2fe7
Diego <diegosantospereira1@gmail.com>
2020-05-07 23:03:44 -0300

    QuaseLá

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,11 @@
 const body = document.querySelector('body');
 const btOpen = document.getElementById('btn-open');
 const closeMenu = document.querySelectorAll('[data-close="menu"]');
+const btnFooter = document.querySelector('#footer button')
+
+btnFooter.addEventListener('click', e =>{
+    window.scrollTo(0, 0)
+})
 
 btOpen.addEventListener('click', e=>{
     body.classList.add('open-menu');
@@ -12,3 +17,9 @@ closeMenu.forEach( el => {
     })
 })
 
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 44.531274, lng: 10.868766},
+        zoom: 14
+    });
+}
